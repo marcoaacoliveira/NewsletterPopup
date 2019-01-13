1c083d5b540a1d188add00c759b0c34748138e87
Marco Oliveira <marcoaacoliveira@gmail.com>
2019-01-13 18:35:29 -0200

    Made partially controller, start the model.

--- magento2/app/code/Redstage/NewsletterPopup/Controller/Newsletter/Save.php
+++ magento2/app/code/Redstage/NewsletterPopup/Controller/Newsletter/Save.php
@@ -1,11 +1,68 @@
 <?php
+
 namespace Redstage\NewsletterPopup\Controller\Newsletter;
+
+use Magento\Customer\Api\AccountManagementInterface as CustomerAccountManagement;
+use Magento\Customer\Model\Session;
+use Magento\Customer\Model\Url as CustomerUrl;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Data\Form\FormKey\Validator;
+use Magento\Framework\Validator\EmailAddress as EmailValidator;
+use Magento\Newsletter\Controller\Subscriber\NewAction;
+use Magento\Newsletter\Model\SubscriberFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Newsletter\Model\Subscriber;
 
 /**
  * Action to save requests to Newsletters
  */
-class Save extends \Magento\Framework\App\Action\Action
+class Save extends NewAction
 {
+
+    protected $formKeyValidator;
+    protected $jsonFactory;
+
+
+    /**
+     * Initialize depedencies.
+     *
+     * @param Context $context
+     * @param SubscriberFactory $subscriberFactory
+     * @param Session $customerSession
+     * @param StoreManagerInterface $storeManager
+     * @param CustomerUrl $customerUrl
+     * @param CustomerAccountManagement $customerAccountManagement
+     * @param EmailValidator|null $emailValidator
+     * @param Validator $formKeyValidator
+     * @param JsonFactory $jsonFactory
+     */
+    public function __construct(
+        Context $context,
+        SubscriberFactory $subscriberFactory,
+        Session $customerSession,
+        StoreManagerInterface $storeManager,
+        CustomerUrl $customerUrl,
+        CustomerAccountManagement $customerAccountManagement,
+        EmailValidator $emailValidator = null,
+        Validator $formKeyValidator,
+        JsonFactory $jsonFactory
+    )
+    {
+
+        $this->formKeyValidator = $formKeyValidator;
+        $this->jsonFactory = $jsonFactory;
+        parent::__construct(
+            $context,
+            $subscriberFactory,
+            $customerSession,
+            $storeManager,
+            $customerUrl,
+            $customerAccountManagement,
+            $emailValidator = null
+        );
+    }
 
     /**
      * Execute save Newsletter on Popup
@@ -14,7 +71,47 @@ class Save extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        return "Oi";
-    }
+        $request = $this->getRequest();
+        $result = $this->jsonFactory->create();
+        $result_json = ['saved' => "false"];
 
+        // Check validate form key
+        if (!$this->formKeyValidator->validate($request)) {
+            $result_json["message"] = __("Something Wrong, try again later");
+            return $result->setData($result_json);
+        }
+
+        // Email is required and must be a post
+        if (!$this->getRequest()->isPost() || !$this->getRequest()->getPost('email')) {
+            $result_json["message"] = __('Email is required');
+            return $result->setData($result_json);
+        }
+
+        $email = (string)$this->getRequest()->getPost('email');
+
+        try {
+            $this->validateEmailFormat($email);
+            $this->validateGuestSubscription();
+            $this->validateEmailAvailable($email);
+
+            $subscriber = $this->_subscriberFactory->create()->loadByEmail($email);
+            if ($subscriber->getId()
+                && (int)$subscriber->getSubscriberStatus() === Subscriber::STATUS_SUBSCRIBED
+            ) {
+                $result_json["message"] = __('This email address is already subscribed.');
+                return $result->setData($result_json);
+            }
+
+            $status = (int)$this->_subscriberFactory->create()->subscribe($email);
+            $result_json["saved"]="true";
+            $result_json["message"]=$status;
+            return $result->setData($result_json);
+        } catch (LocalizedException $e) {
+            $result_json["message"] = __('There was a problem with the subscription: %1', $e->getMessage());
+            return $result->setData($result_json);
+        } catch (\Exception $e) {
+            $result_json["message"] = __('Something went wrong with the subscription.');
+            return $result->setData($result_json);
+        }
+    }
 }
---
+++ magento2/app/code/Redstage/NewsletterPopup/Model/Subscriber.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Redstage\NewsletterPopup\Model;
+
+/**
+ * Override Subscriber to change save and include more
+ *
+ * @package Redstage\NewsletterPopup\Model
+ */
+class Subscriber extends \Magento\Newsletter\Model\Subscriber
+{
+}
